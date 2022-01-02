0178c5930e16e00bec82b0f972e7ccb59bd2b146
msoh <journy002@naver.com>
2022-01-03 02:07:21 +0900

    {}, () 차이

--- src/component/habits.jsx
+++ src/component/habits.jsx
@@ -11,6 +11,8 @@ const Habits = (props) => {
   return (
     <>
       {lists.map((list) => (
+        // '=>' 이후에 {}호를 쓴다면 꼭 return을 써서 값을 반환해줘야 한다.
+        // 바로 값이 나오게 한다면 ()를 사용해주면 된다.
         <Habit list={list} />
       ))}
     </>
